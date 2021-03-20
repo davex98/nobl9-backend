e30bc8e3d69a5c77974f4310a0614b23a1fe2c75
Jakub <jacooob.jb@gmail.com>
2021-03-20 22:09:03 +0100

    added http server + loging

---
+++ random-generator/common/log/logrus.go
@@ -0,0 +1,79 @@
+package log
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&StructuredLogger{logger})
+}
+
+type StructuredLogger struct {
+	Logger *logrus.Logger
+}
+
+func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
+	logFields := logrus.Fields{}
+
+	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
+		logFields["req_id"] = reqID
+	}
+
+	logFields["http_method"] = r.Method
+
+	logFields["remote_addr"] = r.RemoteAddr
+	logFields["uri"] = r.RequestURI
+
+	entry.Logger = entry.Logger.WithFields(logFields)
+
+	entry.Logger.Info("Request started")
+
+	return entry
+}
+
+type StructuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+func (l *StructuredLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"resp_status": status, "resp_bytes_length": bytes,
+		"resp_elapsed": elapsed.Round(time.Millisecond / 100).String(),
+	})
+
+	l.Logger.Info("Request completed	")
+}
+
+func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+		"panic": fmt.Sprintf("%+v", v),
+	})
+}
+
+func Init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "time",
+			logrus.FieldKeyLevel: "severity",
+			logrus.FieldKeyMsg:   "message",
+		},
+	})
+
+	if isLocalEnv, _ := strconv.ParseBool(os.Getenv("LOCAL_ENV")); isLocalEnv {
+		logrus.SetFormatter(&prefixed.TextFormatter{
+			ForceFormatting: true,
+		})
+	}
+
+	logrus.SetLevel(logrus.DebugLevel)
+}
---
+++ random-generator/common/server/http.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"github.com/davex98/nobl9-backend/random-generator/common/log"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+)
+
+func RunHTTPServer(createHandler func(router chi.Router) http.Handler) {
+	RunHTTPServerOnAddr(":"+os.Getenv("PORT"), createHandler)
+}
+
+func RunHTTPServerOnAddr(addr string, createHandler func(router chi.Router) http.Handler) {
+	apiRouter := chi.NewRouter()
+	setMiddlewares(apiRouter)
+
+	logrus.Info("Starting HTTP server")
+
+	http.ListenAndServe(addr, createHandler(apiRouter))
+}
+
+func setMiddlewares(router *chi.Mux) {
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(log.NewStructuredLogger(logrus.StandardLogger()))
+	router.Use(middleware.Recoverer)
+
+	addCorsMiddleware(router)
+
+	router.Use(
+		middleware.SetHeader("X-Content-Type-Options", "nosniff"),
+		middleware.SetHeader("X-Frame-Options", "deny"),
+	)
+	router.Use(middleware.NoCache)
+}
+
+func addCorsMiddleware(router *chi.Mux) {
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	router.Use(corsMiddleware.Handler)
+}
